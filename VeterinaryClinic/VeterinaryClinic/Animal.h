cbb974b3c7b75915e884a1f87911a75a75416f29
karinaolaru <karinaolaru26@gmail.com>
2022-10-20 12:16:19 +0300

    Animal custom matching

--- VeterinaryClinic/VeterinaryClinic/Animal.h
+++ VeterinaryClinic/VeterinaryClinic/Animal.h
@@ -3,12 +3,13 @@
 
 class Animal{
 public:
-	Animal(std::string personalId, std::string name, std::string registrationDate);
+	Animal(uint64_t id,std::string personalId, std::string name, std::string registrationDate);
 	std::string GetPersonalId() const;
 	std::string GetName() const;
 	std::string GetRegistrationDate() const;
 
 protected:
+	uint64_t m_Id;
 	std::string m_PersonalId;
 	std::string m_Name;
 	std::string m_RegistrationDate;
---
+++ VeterinaryClinic/VeterinaryClinic/AnimalType.cpp
@@ -0,0 +1,18 @@
+#include "AnimalType.h"
+
+std::string animalTypeToString(AnimalType type)
+{
+	switch (type)
+	{
+	case AnimalType::HousePet:
+		return "HousePet";
+	case AnimalType::DomesticAnimal:
+		return "DomesticAnimal";
+	case AnimalType::ExoticAnimal:
+		return "ExoticAnimal";
+	case AnimalType::Bird:
+		return "Bird";
+	case AnimalType::Fish:
+		return "Fish";
+	}
+}
